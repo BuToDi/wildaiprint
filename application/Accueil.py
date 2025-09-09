a1c867ebdea89bcd2ff2dc42209e6e6a1b0241e6
Axel.B <axel.bouchayer@gmail.com>
2025-09-09 14:42:47 +0200

    oui

--- application/Accueil.py
+++ application/Accueil.py
@@ -87,9 +87,9 @@ if not st.session_state.location_given:
 
    if location :
        st.session_state.location = location
-       st.stop()
+    st.stop()
    
-st.write(location)
+    st.write(location)
    
 
 
